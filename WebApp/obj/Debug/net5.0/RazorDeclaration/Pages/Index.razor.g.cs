32d1572b304093f2c62b13fb44961c02f2ca0537
langehk <32093132+langehk@users.noreply.github.com>
2021-05-11 17:44:15 +0200

    new location

--- WebApp/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ WebApp/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,91 +13,91 @@ namespace WebApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 1 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 2 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 3 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 4 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 5 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 6 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 7 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 8 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 9 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using WebApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 10 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using WebApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 11 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using WebApp.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 12 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using WebApp.Dto;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "C:\Users\Morten\source\repos\BlazorGolf\WebApp\_Imports.razor"
+#line 14 "C:\Users\Morten\Documents\GitHub\BlazorGolf\WebApp\_Imports.razor"
 using MudBlazor;
 
 #line default
